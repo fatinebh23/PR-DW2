3d51f8de3fda35911bcf21dac59d932a2606fcb7
daw2 <fatinebh23@gmail.com>
2023-12-11 12:15:52 +0100

    api Funciona

--- OpenApi/src/main/java/org/example/Team.java
+++ OpenApi/src/main/java/org/example/Team.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+//ignorar las otras etiquetas
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Team {
     private String strTeam;
